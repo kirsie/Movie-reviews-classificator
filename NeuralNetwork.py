2ac033699f91dda682212660a6b607b6f8952936
Wrobelek <wojwrobel@wp.pl>
2019-04-09 23:44:02 +0100

    cleaning

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,11 +2,12 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras import layers
 
-class Data:
+
+class NeuralNetwork:
 
     train_input = []
     train_output = []
@@ -28,7 +29,7 @@ class Data:
         reviews = pd.concat(reviews)
         return reviews
 
-    def split__data(self, reviews):
+    def split_data(self, reviews):
 
         df_yelp = reviews[reviews['source'] == 'yelp']
         df_amazon = reviews[reviews['source'] == 'amazon']
@@ -58,7 +59,7 @@ class Data:
     def simple_neural_network(self):
 
         input_dim = self.train_input.shape[1]
-        print(input_dim)
+
         model = Sequential()
         model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
         model.add(layers.Dense(1, activation='sigmoid'))
@@ -66,18 +67,43 @@ class Data:
         model.summary()
 
         history = model.fit(self.train_input, self.train_output,
-                            epochs=200,
-                            validation_data=(self.test_input, self.test_output),
-                            batch_size=1)
+                            epochs=200, validation_data=(self.test_input, self.test_output),
+                            batch_size=300)
 
         loss, accuracy = model.evaluate(self.train_input, self.train_output, verbose=False)
         print("Training Accuracy: {:.4f}".format(accuracy))
         loss, accuracy = model.evaluate(self.test_input, self.test_output, verbose=False)
         print("Testing Accuracy:  {:.4f}".format(accuracy))
 
-data = Data()
-reviews = data.import_reviews_to_list()
-data.split__data(reviews)
-data.vectorize_string_data()
-data.logistic_regression()
-data.simple_neural_network()
+        self.plot_results(history)
+
+    @staticmethod
+    def plot_results(history):
+
+        training_accuracy = history.history['acc']
+        testing_accuracy = history.history['val_acc']
+
+        training_loss = history.history['loss']
+        testing_loss = history.history['val_loss']
+
+        x = range(1, len(training_accuracy)+1)
+
+        plt.figure(figsize=(12, 5))
+        plt.subplot(1, 2, 1)
+        plt.plot(x, training_accuracy, 'b', label='Training acc')
+        plt.plot(x, testing_accuracy, 'r', label='Validation acc')
+        plt.title('Training and validation accuracy')
+        plt.legend()
+        plt.subplot(1, 2, 2)
+        plt.plot(x, training_loss, 'b', label='Training loss')
+        plt.plot(x, testing_loss, 'r', label='Validation loss')
+        plt.title('Training and validation loss')
+        plt.legend()
+        plt.show()
+
+neural_network = NeuralNetwork()
+reviews = neural_network.import_reviews_to_list()
+neural_network.split_data(reviews)
+neural_network.vectorize_string_data()
+neural_network.logistic_regression()
+neural_network.simple_neural_network()
